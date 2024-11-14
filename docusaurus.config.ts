ea903f99eda510de9c101dbd8eb9d7e45bac2cbb
Rory <rwalsh@Rorys-Mac-mini.fritz.box>
2024-11-14 18:43:42 +0000

    updating

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://rorywalsh.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cabbage3website/docs/intro/',
+  baseUrl: '/cabbage3website/',
   deploymentBranch: 'gh-pages',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
